668f0d24a422522e6d0795983eeecc8234ff12f3
Henri Niva <hegemege@gmail.com>
2018-12-11 21:24:22 +0200

    Day 1 improvements

--- 01/01.py
+++ 01/01.py
@@ -2,14 +2,14 @@
 def main():
     data = read_input()
     # Part 1
-    print(sum(map(lambda x: int(x), data)))
+    print(sum(data))
 
     # Part 2
     seen_frequencies = set()
     current_frequency = 0
     while True:
-        for item in data:
-            current_frequency += int(item)
+        for value in data:
+            current_frequency += value
             if current_frequency in seen_frequencies:
                 print(current_frequency)
                 return
@@ -19,7 +19,7 @@ def main():
 # Read the file and remove trailing new line characters
 def read_input():
     f = open('input.txt', 'r')
-    data = list(map(lambda x: x[:-1], f.readlines()))
+    data = list(map(lambda x: int(x[:-1]), f.readlines()))
     f.close()
     return data
 
